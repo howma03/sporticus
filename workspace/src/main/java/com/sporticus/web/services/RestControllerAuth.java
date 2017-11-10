84dec7eb68f0721837e510c649f65aef3ceb2c71
MarkHo <mark.howell@microfocus.com>
2017-11-10 11:25:36 +0000

    added a post rest call for authenication

--- workspace/src/main/java/com/sporticus/web/services/RestControllerAuth.java
+++ workspace/src/main/java/com/sporticus/web/services/RestControllerAuth.java
@@ -1,8 +1,5 @@
 package com.sporticus.web.services;
 
-import com.sporticus.domain.entities.User;
-import com.sporticus.domain.repositories.IRepositoryUser;
-import com.sporticus.interfaces.IServiceUser;
 import com.sporticus.util.logging.LogFactory;
 import com.sporticus.util.logging.Logger;
 import com.sporticus.web.controllers.ControllerAbstract;
@@ -14,16 +11,15 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/papi/user")
-public class RestControllerUser extends ControllerAbstract {
+public class RestControllerAuth extends ControllerAbstract {
 
-    private static final Logger LOGGER = LogFactory.getLogger(RestControllerUser.class.getName());
+    private static final Logger LOGGER = LogFactory.getLogger(RestControllerAuth.class.getName());
 
     private static final BCryptPasswordEncoder ENCODER = new BCryptPasswordEncoder();
 
@@ -31,13 +27,13 @@ public class RestControllerUser extends ControllerAbstract {
     private UserDetailsService serviceDetails;
 
     @Autowired
-    public RestControllerUser(final UserDetailsService serviceDetails) {
+    public RestControllerAuth(final UserDetailsService serviceDetails) {
         this.serviceDetails = serviceDetails;
     }
 
     @RequestMapping(value = "/authenticate/{username}/{password}", method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> authenticate(@PathVariable("username") String userName,
+    public ResponseEntity<String> authenticateGet(@PathVariable("username") String userName,
                                                @PathVariable("password") String password) {
 
         LOGGER.debug(() -> "GET:/papi/user/authenticate");
@@ -52,5 +48,26 @@ public class RestControllerUser extends ControllerAbstract {
         }
 
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/authenticate/{username}/{password}", method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> authenticate(@PathVariable("username") String userName,
+                                               @PathVariable("password") String password) {
+
+        LOGGER.debug(() -> "POST:/papi/user/authenticate");
+
+        final UserDetails userDetails = serviceDetails.loadUserByUsername(userName);
+
+        if(!ENCODER.matches(password, userDetails.getPassword())){
+
+            LOGGER.info(()->"Passwords do not match");
+
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
+        String JSON = "";
+
+        return new ResponseEntity<>(JSON, HttpStatus.OK);
     }
 }
