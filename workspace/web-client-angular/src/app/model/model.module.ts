78362fc9aef8f183a6a160c58f43510e7c2e5de4
Bob Tarling <bob.tarling@microfocus.com>
2017-11-13 13:30:40 +0000

    Move user model

---
+++ workspace/web-client-angular/src/app/model/model.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {User} from './user';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+//    User
+  ],
+  declarations: [
+//    User
+  ]
+})
+export class ModelModule { }
