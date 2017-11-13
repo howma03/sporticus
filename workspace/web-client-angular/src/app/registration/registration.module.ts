3d4dc28a36590ab5c4d8988dbd303611e4c28aff
jamesn <james.nurse@microfocus.com>
2017-11-13 16:13:59 +0000

    Add initial page look functionality.

--- workspace/web-client-angular/src/app/registration/registration.module.ts
+++ workspace/web-client-angular/src/app/registration/registration.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import {WizardModule} from "@ux-aspects/ux-aspects";
+import {MatInputModule} from "@angular/material";
 
 @NgModule({
   imports: [
     CommonModule,
+    MatInputModule,
     WizardModule
   ],
   declarations: [RegisterComponent]
