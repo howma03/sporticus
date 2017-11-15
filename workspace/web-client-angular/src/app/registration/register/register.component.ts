4d10d9c03f39fc09b9ab281851b99a838df3aa23
jamesn <james.nurse@microfocus.com>
2017-11-15 15:09:38 +0000

    Get registering working nicely

--- workspace/web-client-angular/src/app/registration/register/register.component.ts
+++ workspace/web-client-angular/src/app/registration/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {RegisterUser, RegistrationService} from "../../services/registration.service";
 import {ErrorHandlingService} from "../../services/error-handling.service";
 import {Router} from "@angular/router";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-register',
@@ -30,7 +31,8 @@ export class RegisterComponent implements OnInit {
   doRegister() {
     let testUser: RegisterUser = {
       userName: this.registrationDetails.userName,
-      name: this.registrationDetails.name,
+      firstName: this.registrationDetails.firstName,
+      surname: this.registrationDetails.surname,
       email: this.registrationDetails.email,
       password: this.registrationDetails.password
     };
@@ -38,10 +40,16 @@ export class RegisterComponent implements OnInit {
     this.registrationService.createOne(testUser)
       .subscribe(success => {
         if (success) {
-          alert("User " + testUser.name +  " successfully created.")
+          alert("User " + testUser.firstName + " " + testUser.surname +  " successfully created.")
           this.router.navigate(['/landing/login']);
         }
       }, err => {
+        if(err instanceof HttpErrorResponse) {
+          if(err.status === 400) {
+            alert("A user is already registered with the email=" + testUser.firstName + " " + testUser.surname)
+          }
+        }
+
         this.errorHandlingService.handleError(err);
 
         //this.loading = false;
