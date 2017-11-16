0663dbc2d8f87562c65a101ad8b328b60ead1575
jamesn <james.nurse@microfocus.com>
2017-11-16 16:15:13 +0000

    Tiny styling change to font page

--- workspace/web-client-angular/src/app/landing/initial/initial.component.ts
+++ workspace/web-client-angular/src/app/landing/initial/initial.component.ts
@@ -13,7 +13,7 @@ export class InitialComponent implements OnInit {
     grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
     slide: 1,
     speed: 400,
-    interval: 4000,
+    //interval: 4000,
     point: {
       visible: true
     },
