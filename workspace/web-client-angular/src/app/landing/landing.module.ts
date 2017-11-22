5db6f044dfc143ff6d06d416c0ba89a6ca652a59
MarkHo <mark.howell@microfocus.com>
2017-11-22 15:04:53 +0000

    extended rest end-point for emitter function

--- workspace/web-client-angular/src/app/landing/landing.module.ts
+++ workspace/web-client-angular/src/app/landing/landing.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LandingComponent } from './landing/landing.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LandingComponent} from './landing/landing.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
 import {ColorServiceModule, DashboardModule, EboxModule, PageHeaderModule, SparkModule} from "@ux-aspects/ux-aspects";
 import {LandingRoutingModule} from "./landing-routing.module";
-import { AboutComponent } from './about/about.component';
-import { LegalComponent } from './legal/legal.component';
-import { ContactComponent } from './contact/contact.component';
-import { ClubsComponent } from './clubs/clubs.component';
-import { HelpComponent } from './help/help.component';
-import { OverviewComponent } from './overview/overview.component';
-import { LegalDisclosurePolicyComponent } from './legal-disclosure-policy/legal-disclosure-policy.component';
-import { InitialComponent } from './initial/initial.component';
+import {AboutComponent} from './about/about.component';
+import {LegalComponent} from './legal/legal.component';
+import {ContactComponent} from './contact/contact.component';
+import {ClubsComponent} from './clubs/clubs.component';
+import {HelpComponent} from './help/help.component';
+import {OverviewComponent} from './overview/overview.component';
+import {LegalDisclosurePolicyComponent} from './legal-disclosure-policy/legal-disclosure-policy.component';
+import {InitialComponent} from './initial/initial.component';
 import {NgxCarouselModule} from "ngx-carousel";
-import { PricesComponent } from './prices/prices.component';
+import {PricesComponent} from './prices/prices.component';
 
 @NgModule({
   imports: [
@@ -27,6 +27,7 @@ import { PricesComponent } from './prices/prices.component';
     SparkModule,
     LandingRoutingModule
   ],
+  exports: [FooterComponent],
   declarations: [LandingComponent, HeaderComponent, FooterComponent, AboutComponent, LegalComponent, ContactComponent, ClubsComponent, HelpComponent, OverviewComponent, LegalDisclosurePolicyComponent, InitialComponent, PricesComponent ]
 })
 export class LandingModule { }
