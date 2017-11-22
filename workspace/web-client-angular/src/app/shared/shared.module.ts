8a1074ccf306e8b7db23ee0806438db580d7d8c5
Thomas Hardwick <thomas.hardwick@microfocus.com>
2017-11-22 10:39:31 +0000

    Create a Base Widget Component

--- workspace/web-client-angular/src/app/shared/shared.module.ts
+++ workspace/web-client-angular/src/app/shared/shared.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
-import {MatButtonModule, MatDialogModule} from '@angular/material';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ConfirmDialogComponent} from './confirm-dialog/confirm-dialog.component';
+import {MatButtonModule, MatDialogModule, MatToolbarModule} from '@angular/material';
+import {WidgetComponent} from "./widget/widget.component";
 
 @NgModule({
   imports: [
     CommonModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
-  declarations: [ConfirmDialogComponent],
+  exports: [
+    WidgetComponent
+  ],
+  declarations: [ConfirmDialogComponent, WidgetComponent],
   entryComponents: [ConfirmDialogComponent]
 })
 export class SharedModule { }
